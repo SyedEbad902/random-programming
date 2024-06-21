db8d27f39c3dce942e7e694ad32044740fa9fbd7
SyedEbad902 <ebad3088@gmail.com>
2024-06-22 00:51:37 +0500

    new question solved

---
+++ ques17.py
@@ -0,0 +1,8 @@
+# Check if a Number is Even or Odd
+def is_even_or_odd(n):
+    return "Even" if n % 2 == 0 else "Odd"
+
+
+number = 7
+even_or_odd_result = is_even_or_odd(number)
+print(even_or_odd_result)  

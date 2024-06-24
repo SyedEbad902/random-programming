e0dd06da970a5dd938a37235dabb1e49ad20a690
SyedEbad902 <ebad3088@gmail.com>
2024-06-25 01:32:21 +0500

    new question solved

---
+++ ques19.py
@@ -0,0 +1,14 @@
+def sieve_of_eratosthenes(n):
+    primes = [True] * (n + 1)
+    p = 2
+    while (p * p <= n):
+        if primes[p] == True:
+            for i in range(p * p, n + 1, p):
+                primes[i] = False
+        p += 1
+    prime_numbers = [p for p in range(2, n + 1) if primes[p]]
+    return prime_numbers
+
+n = 30
+prime_numbers_result = sieve_of_eratosthenes(n)
+prime_numbers_result
